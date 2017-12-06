1bb9249f6c9f5b8f9e6dd83b0a13cf7660a92c82
Fujii daisuke <ifc.dai.faculty@gmail.com>
2017-12-07 01:44:19 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 8000));
+// JSONの送信を許可
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// JSONのパースを楽に（受信時）
+app.use(bodyParser.json({
+    verify(req, res, buf) {
+        req.rawBody = buf
+    }
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use('/callback',botman);
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running');
+});
